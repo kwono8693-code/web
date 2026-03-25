778dfde0c2d0a74fc7b7d6767826f3211b09e08f
권오영 <kwono8693@naver.com>
2026-03-25 15:03:27 +0900

    260325 제품 상세 페이지 작업

---
+++ script/detail-list.js
@@ -0,0 +1,35 @@
+let listArray = [
+        {
+            pid:0,
+            pName:"Amber Musk Night",
+            pDesc:"따뜻한 앰버와 부드러운 머스크",
+            pPrice:39000,
+            pThumbnail:"menu-list2.jpg",
+            plike:false,
+        },
+        {
+            pid:1,
+            pName:"Fresh Citrus Bloom",
+            pDesc:"상큼한 시트러스 향",
+            pPrice:34900,
+            pThumbnail:"menu-list3.jpg",
+            plike:false,
+        },
+        {
+            pid:2,
+            pName:"White Tea Linen",
+            pDesc:"맑은 화이트 티와 깨끗한 리넨 향",
+            pPrice:34900,
+            pThumbnail:"menu-list4.jpg",
+            plike:false,
+        },
+        {
+            pid:3,
+            pName:"Pure Cotton",
+            pDesc:"순수한 코튼처럼 가볍고 편안한 촉촉함",
+            pPrice:29000,
+            pThumbnail:"menu-list5.jpg",
+            plike:false,
+        },
+        
+    ]
